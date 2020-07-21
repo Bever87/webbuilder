c4d14e9e6cfcfb2887c7271e0aa03a9c9e84ae1c
Thomas Van Beveren <thomas.vanbeveren@realdolmen.com>
2020-07-21 14:31:21 +0200

    menu styling done + language buttons

--- libs/shared/layouts/src/lib/shared-layouts.module.ts
+++ libs/shared/layouts/src/lib/shared-layouts.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedMainLayoutComponent } from './main-layout/main-layout.component';
 import { SharedMainLayoutHeaderComponent } from './main-layout/header/main-layout-header.component';
 import { SharedMainLayoutMenuComponent } from './main-layout/menu/main-layout-menu.component';
+import { NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [SharedMainLayoutComponent, SharedMainLayoutHeaderComponent, SharedMainLayoutMenuComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NgbTooltipModule],
   exports: [SharedMainLayoutComponent]
 })
 export class SharedLayoutsModule {}
