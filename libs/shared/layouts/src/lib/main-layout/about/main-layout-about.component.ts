4bbb372f68ebbb3b8a4ca30a14ac344d487c3532
Thomas Van Beveren <bever@solvedit.be>
2020-07-23 21:24:58 +0200

    about us

---
+++ libs/shared/layouts/src/lib/main-layout/about/main-layout-about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'solved-ml-about',
+    templateUrl: './main-layout-about.component.html',
+    styleUrls: ['./main-layout-about.component.scss']
+})
+export class SharedMainLayoutAboutComponent {
+
+}
--- libs/shared/layouts/src/lib/shared-layouts.module.ts
+++ libs/shared/layouts/src/lib/shared-layouts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedMainLayoutComponent } from './main-layout/main-layout.component';
 import { SharedMainLayoutHeaderComponent } from './main-layout/header/main-layout-header.component';
+import { SharedMainLayoutAboutComponent } from './main-layout/about/main-layout-about.component';
 import { SharedMainLayoutMenuComponent } from './main-layout/menu/main-layout-menu.component';
 import { NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
 import { SharedSearchModule } from '@solved/shared/search';
 
 @NgModule({
-  declarations: [SharedMainLayoutComponent, SharedMainLayoutHeaderComponent, SharedMainLayoutMenuComponent],
+  declarations: [SharedMainLayoutComponent, SharedMainLayoutHeaderComponent, SharedMainLayoutMenuComponent, SharedMainLayoutAboutComponent],
   imports: [CommonModule, NgbTooltipModule, SharedSearchModule],
   exports: [SharedMainLayoutComponent]
 })
