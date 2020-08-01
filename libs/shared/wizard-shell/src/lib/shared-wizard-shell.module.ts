23e103cc3ba6e7f192daec6357bad89b1f05edcf
Thomas Van Beveren <bever@solvedit.be>
2020-08-01 15:26:14 +0200

    make resizable actions panel

--- libs/shared/wizard-shell/src/lib/shared-wizard-shell.module.ts
+++ libs/shared/wizard-shell/src/lib/shared-wizard-shell.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedWizardShellComponent } from './shared-wizard-shell/shared-wizard-shell.component';
+import { ResizableModule } from 'angular-resizable-element';
 
 @NgModule({
   declarations: [SharedWizardShellComponent],
    exports: [SharedWizardShellComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ResizableModule],
 })
 export class SharedWizardShellModule {}
--- libs/shared/wizard-shell/src/lib/shared-wizard-shell/shared-wizard-shell.component.ts
+++ libs/shared/wizard-shell/src/lib/shared-wizard-shell/shared-wizard-shell.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
     selector: 'solved-shared-wiz-shell',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
     styleUrls: ['./shared-wizard-shell.component.scss']
 })
 export class SharedWizardShellComponent {
+    right = 350;
+    minlimit = 100;
+    maxlimit = 750;
 
+    onResizeEnd(event: ResizeEvent): void {
+        if (event.edges.right) {
+            const checkValue = this.right + (+event.edges.right);
+            if (checkValue < this.minlimit) {
+                this.right = this.minlimit;
+            } else {
+                if (checkValue > this.maxlimit) {
+                    this.right = this.maxlimit
+                } else {
+                    this.right += +event.edges.right;
+                }
+            }
+        }
+    }
 }
